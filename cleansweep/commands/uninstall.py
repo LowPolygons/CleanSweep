3510a472a40f7d2e888711be7117f674ba1b071d
Tristan Rimmer <tristan.rimmer@stfc.ac.uk>
2025-08-06 15:17:07 +0100

    Implemented uninstall command

--- cleansweep/commands/uninstall.py
+++ cleansweep/commands/uninstall.py
@@ -1,10 +1,34 @@
+from pathlib import Path
+from cleansweep.globals.storage_paths import StoragePaths
 from cleansweep.interfaces.command import CommandInterface 
 from argparse import Namespace, _SubParsersAction
+
+import shutil
 
 class UninstallCommand(CommandInterface):
     @staticmethod
     def command(args: Namespace) -> None:
         print("Removing the stored data from CleanSweep")
+
+        user_home_dir: Path = Path.home()
+        storage_path: StoragePaths = StoragePaths(user_home_dir)
+
+        # Create the main cleansweep dir
+        main_folder: Path = user_home_dir / storage_path.main_dir_name
+        
+        # Create the main folder if it doesn't exist 
+        if not main_folder.exists():
+            print("CleanSweep has not been initialised")
+            return
+    
+        # Confirm removal
+        confirmation = input("If you are certain you want to remove CleanSweep, enter 'cleansweep uninstall' again\n => ")
+
+        # Remove the cleansweep directory
+        if confirmation == "cleansweep uninstall":
+            shutil.rmtree(main_folder)
+            print("Uninstalled cleansweep data from your system - to rebuild, run 'cleansweep setup'")
+        
 
     @classmethod
     def register_subparser(cls, subparsers: _SubParsersAction) -> None:
