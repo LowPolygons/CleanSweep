e659fe556f895b59111867ad6b4cb81e6b65e285
Tristan Rimmer <Tristan Rimmer>
2026-02-06 14:20:42 +0000

    Added JsonIO file

---
+++ src/systems/json_io.rs
@@ -0,0 +1,41 @@
+use std::path::{Path, PathBuf};
+
+use serde::{Serialize, de::DeserializeOwned};
+use std::fs::File;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum JsonIOError {
+    #[error("When reading a path variable, it errored turning result to string")]
+    FsReadToStringError,
+
+    #[error("Serde Json errored when interpreting string as struct")]
+    SerdeJsonFromStrError,
+
+    #[error("Failed when trying to create a File object from provided Path")]
+    FileCreateFromPathError,
+
+    #[error("Serde Json errored trying to convert the passed struct into pretty json")]
+    SerdeJsonWritePrettyError,
+}
+
+pub fn read_file_to_struct<T: DeserializeOwned, P: AsRef<Path>>(path: P) -> Result<T, JsonIOError> {
+    let stringy_json =
+        std::fs::read_to_string(path).map_err(|_| JsonIOError::FsReadToStringError)?;
+    let structy_value =
+        serde_json::from_str(&stringy_json).map_err(|_| JsonIOError::SerdeJsonFromStrError)?;
+
+    Ok(structy_value)
+}
+
+pub fn write_json_file_from_struct<T: Serialize, P: AsRef<Path>>(
+    object: &T,
+    path: P,
+) -> Result<(), JsonIOError> {
+    let file = File::create(path).map_err(|_| JsonIOError::FileCreateFromPathError)?;
+
+    serde_json::to_writer_pretty(file, object)
+        .map_err(|_| JsonIOError::SerdeJsonWritePrettyError)?;
+
+    Ok(())
+}
