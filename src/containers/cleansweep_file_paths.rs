d88bd614eee441e2bf34a04ccf90aeb56e7e40f5
Tristan Rimmer <Tristan Rimmer>
2026-02-10 10:56:45 +0000

    File Paths updated to have a specified file type for better handling

--- src/containers/cleansweep_file_paths.rs
+++ src/containers/cleansweep_file_paths.rs
@@ -1,27 +1,37 @@
-pub enum CleansweepFilePaths {
-    MainDirectoryName, // All stored as seen here in $HOME/
-    ToDelete,
-    ToKeep,
-    UserSettings,
-    LogFile,
-    UserSettingsDefault,
-    ToDeleteLocalTemp,
-    ToKeepLocalTemp,
-    FoundSets,
+pub enum FileStyle {
+    Json(&'static str),
+    Txt(&'static str),
+    PathOnly(&'static str),
+    NoExtension(&'static str),
 }
 
+pub enum CleansweepFilePaths {
+    MainDirectoryName(FileStyle), // All stored as seen here in $HOME/
+    ToDelete(FileStyle),
+    ToKeep(FileStyle),
+    UserSettings(FileStyle),
+    LogFile(FileStyle),
+    UserSettingsDefault(FileStyle),
+    ToDeleteLocalTemp(FileStyle),
+    ToKeepLocalTemp(FileStyle),
+    FoundSets(FileStyle),
+    FilterComponentList(FileStyle),
+}
+
+#[rustfmt::skip]
 impl CleansweepFilePaths {
-    pub fn name(self) -> &'static str {
+    pub fn name(self) -> FileStyle {
         match self {
-            CleansweepFilePaths::MainDirectoryName => ".cleansweep",
-            CleansweepFilePaths::ToDelete => "to_delete_files.json",
-            CleansweepFilePaths::ToKeep => "to_keep_files.json",
-            CleansweepFilePaths::UserSettings => "user_settings.json",
-            CleansweepFilePaths::UserSettingsDefault => "user_settings_default.json",
-            CleansweepFilePaths::LogFile => "log.txt",
-            CleansweepFilePaths::ToDeleteLocalTemp => "STAGED_FOR_DELETION",
-            CleansweepFilePaths::ToKeepLocalTemp => "FILES_KEPT_SANITY_CHECK",
-            CleansweepFilePaths::FoundSets => "found_sets.json",
+            CleansweepFilePaths::MainDirectoryName(_) => FileStyle::PathOnly(".cleansweep"),
+            CleansweepFilePaths::ToDelete(_) => FileStyle::Json("to_delete_files.json"),
+            CleansweepFilePaths::ToKeep(_) => FileStyle::Json("to_keep_files.json"),
+            CleansweepFilePaths::UserSettings(_) => FileStyle::Json("user_settings.json"),
+            CleansweepFilePaths::UserSettingsDefault(_) => FileStyle::Json("user_settings_default.json"),
+            CleansweepFilePaths::LogFile(_) => FileStyle::Txt("log.txt"),
+            CleansweepFilePaths::ToDeleteLocalTemp(_) => FileStyle::NoExtension("STAGED_FOR_DELETION"),
+            CleansweepFilePaths::ToKeepLocalTemp(_) => FileStyle::NoExtension("FILES_KEPT_SANITY_CHECK"),
+            CleansweepFilePaths::FoundSets(_) => FileStyle::Json("found_sets.json"),
+            CleansweepFilePaths::FilterComponentList(_) => FileStyle::Txt("filter_components.txt"),
         }
     }
 }
