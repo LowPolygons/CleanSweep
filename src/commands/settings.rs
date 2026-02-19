9a51edee5872028861f55e5b24d42b658fa16f3d
Tristan Rimmer <Tristan Rimmer>
2026-02-19 13:26:01 +0000

    Implemented settings command

--- src/commands/settings.rs
+++ src/commands/settings.rs
@@ -1,6 +1,89 @@
-use crate::cli::SettingsArgs;
+use std::{
+    env::{VarError, temp_dir},
+    fs::File,
+};
+
+use crate::{
+    cli::SettingsArgs,
+    containers::{
+        cleansweep_file_paths::CleansweepFilePaths,
+        user_settings::{self, UserSettings, get_user_setting_lines},
+    },
+    systems::json_io::{read_file_to_struct, write_json_file_from_struct},
+    utils::{create_defaults::create_default_user_settings, get_home_dir::get_cleansweep_dir},
+};
 
 pub fn settings(args: &SettingsArgs) -> Result<(), String> {
-    println!("hello from settings {:?}", args);
-    Ok(())
+    let cleansweep_dir = get_cleansweep_dir().map_err(|e| format!("{}", e))?;
+
+    match args {
+        SettingsArgs::Reset => {
+            let defalt_user_settings = create_default_user_settings();
+
+            write_json_file_from_struct(
+                &defalt_user_settings,
+                cleansweep_dir.join(CleansweepFilePaths::UserSettings.name()),
+            )
+            .map_err(|e| format!("{}", e))?;
+
+            return Ok(());
+        }
+        SettingsArgs::Modify => {
+            let editor = match std::env::var("EDITOR") {
+                Ok(val) => Ok(val),
+                Err(err) => {
+                    let result = match err {
+                        VarError::NotPresent => {
+                            if std::fs::exists("/usr/bin/nano").map_err(|e| {
+                                format!("Couldnt validate /usr/bin/nano directory, {e}")
+                            })? {
+                                println!("No EDITOR var set, defaulting to nano");
+                                Ok("/usr/bin/nano".to_string())
+                            } else if std::fs::exists("/usr/bin/vi").map_err(|e| {
+                                format!("Couldnt validate /usr/bin/vi directory, {e}")
+                            })? {
+                                println!("No EDITOR var set, couldn't find nano, using vi");
+                                Ok("/usr/bin/vi".to_string())
+                            } else {
+                                Err("Could not find backup editor nano or vi".to_string())
+                            }
+                        }
+                        VarError::NotUnicode(e) => Err(format!(
+                            "Couldnt get EDITOR var due to it not being Unicode, {e:?}"
+                        )),
+                    };
+                    result
+                }
+            }
+            .map_err(|e| format!("Failed to access the $EDITOR var, {e:?}"))?;
+
+            let user_settings: UserSettings =
+                read_file_to_struct(cleansweep_dir.join(CleansweepFilePaths::UserSettings.name()))
+                    .map_err(|e| format!("{e}"))?;
+
+            std::process::Command::new(editor)
+                .arg(&cleansweep_dir.join(CleansweepFilePaths::UserSettings.name()))
+                .status()
+                .map_err(|e| format!("Failed to edit file {e}"))?;
+
+            println!("Whatever changes you made direclty affected the user_settings file.");
+            println!(
+                "If a new command attempts to open it and fails, consider running 'cleansweep settings reset"
+            );
+
+            return Ok(());
+        }
+
+        SettingsArgs::Display => {
+            let user_settings: UserSettings =
+                read_file_to_struct(cleansweep_dir.join(CleansweepFilePaths::UserSettings.name()))
+                    .map_err(|e| format!("{e}"))?;
+
+            for line in get_user_setting_lines(user_settings) {
+                println!("{}", line);
+            }
+
+            Ok(())
+        }
+    }
 }
--- src/containers/user_settings.rs
+++ src/containers/user_settings.rs
@@ -107,3 +107,32 @@ impl PerFilterOptions {
         &self.accessed_after
     }
 }
+
+#[rustfmt::skip]
+pub fn get_user_setting_lines(user_settings: UserSettings) -> Vec<String> {
+    let keeps = user_settings.get_to_keep_list();
+    let deletes = user_settings.get_to_delete_list();
+    let sets = user_settings.get_set_scan_option();
+
+    let mut lines: Vec<String> = Vec::new();
+
+    lines.push(format!("Keep Files..."));
+    lines.push(format!(".. with extension {:?}", keeps.get_extensions()));
+    lines.push(format!(".. whose name is or contains {:?}", keeps.get_name()));
+    lines.push(format!(".. whose directory contains {:?}", keeps.get_directory()));
+    lines.push(format!(".. whose larger than {}", keeps.get_larger_than_size()));
+    lines.push(format!(".. whose smaller than {}", deletes.get_larger_than_size()));
+    
+    lines.push(format!("Delete Files..."));
+    lines.push(format!(".. with extension {:?}", deletes.get_extensions()));
+    lines.push(format!(".. whose name is or contains {:?}", deletes.get_name()));
+    lines.push(format!(".. whose directory contains {:?}", deletes.get_directory()));
+    lines.push(format!(".. whose larger than {}", deletes.get_larger_than_size()));
+    lines.push(format!(".. whose smaller than {}", keeps.get_larger_than_size()));
+
+    lines.push(format!("Files which may be in sets.. "));
+    lines.push(format!(".. have extension {:?}", sets.get_with_extension()));
+    lines.push(format!(".. have name containing {:?}", sets.get_name_contains()));
+    
+    lines
+}
