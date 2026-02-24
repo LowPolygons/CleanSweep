193c6be67a6e292a64efecc3d54527442bf21846
Tristan Rimmer <Tristan Rimmer>
2026-02-24 10:45:49 +0000

    Fully implemented demolish command, removing its mock delete function

--- src/commands/demolish.rs
+++ src/commands/demolish.rs
@@ -1,29 +1,29 @@
-use std::path::Path;
+use std::{fs, path::Path};
 
 use rand::RngExt;
 
-use crate::utils::get_home_dir::get_cleansweep_dir;
+use crate::utils::{
+    get_home_dir::get_cleansweep_dir,
+    run_time_user_input::get_string_input_matching_provided_string,
+};
 
 pub fn demolish() -> Result<(), String> {
     let cleansweep_dir = get_cleansweep_dir().map_err(|e| format!("{e}"))?;
 
-    // fs::remove_dir_all(cleansweep_dir)
+    let has_confirmed = get_string_input_matching_provided_string(
+        "To carry out demolish, please write \"cleansweep demolish\"",
+        "cleansweep demolish",
+    )
+    .map_err(|e| format!("{e}"))?;
 
-    MOCK_DEMOLISH(&cleansweep_dir)
-        .map_err(|e| format!("Failed to delete .cleansweep directory: {e}"))?;
+    if has_confirmed {
+        fs::remove_dir_all(cleansweep_dir)
+            .map_err(|e| format!("Failed to remove everything from the directory, {}", e))?;
 
-    println!("Deleted the .cleansweep directory");
+        println!("Deleted the .cleansweep directory");
+    } else {
+        println!("You did not provide the correct string to carry out the demolish");
+    }
 
     Ok(())
-}
-// TODO: cargo remove rand
-fn MOCK_DEMOLISH(_: &Path) -> Result<(), String> {
-    let mut rng = rand::rng();
-    let number = rng.random_range(1..=100);
-
-    if number > 99 {
-        return Err("Pretend fail".to_string());
-    } else {
-        return Ok(());
-    }
 }
